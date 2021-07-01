5f8e1217dc6a1e82687a76c7f1baea62caeba140
Sobrino Julian <sobrinojulian@pm.me>
2021-07-01 10:30:43 -0300

    Ordeno

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,30 @@
 module.exports = function (config) {
   config.addPassthroughCopy("assets");
 };
+const minifyHtml = require('./src/minifyHtml')
+const browsersyncConfig = require('./src/browsersyncConfig')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    'assets': 'assets',
+    'netlifycms.yml': 'admin/config.yml',
+    'site/favicon.ico': 'favicon.ico',
+    'node_modules/alpinejs/dist/cdn.min.js': 'alpine.min.js',
+    'node_modules/@fortawesome/fontawesome-free/sprites': 'static/sprites',
+  })
+
+  if (isProduction) {
+    eleventyConfig.addTransform('minifyHtml', minifyHtml)
+  } else {
+    eleventyConfig.setBrowserSyncConfig(browsersyncConfig)
+  }
+
+  return {
+    dir: {
+      input: 'site',
+      output: 'dist',
+    },
+  }
+}
